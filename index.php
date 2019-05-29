9bd8abcb881e620bccd8944992920c7de3aea306
Silvia Salete <silvia.samtech@gmail.com>
2019-05-29 14:19:23 -0300

    Commit

--- index.php
+++ index.php
@@ -29,9 +29,6 @@
                                    <li class="nav-item">
                                         <a class="nav-link text-white" href="enviar-email.php" tabindex="-1">E-MAIL</a>
                                    </li>
-                                   <li class="nav-item">
-                                        <a class="nav-link text-white" href="novo-excel.php" tabindex="-1">Novo Excel</a>
-                                   </li>
                          </ul>
                     </nav>
                </header>               
